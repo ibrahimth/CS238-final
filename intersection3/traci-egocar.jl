580483a3dde3caa267b94ea6713c810f9949b27e
johnldean <deanjl@stanford.edu>
2016-12-04 22:17:47 -0800

    ego car code now runs and does reward correctly

--- intersection3/traci-egocar.jl
+++ intersection3/traci-egocar.jl
@@ -11,72 +11,71 @@ pos_i = net[:getNode]("center")[:getCoord]()
 n_tracked_cars = 2
 timestep = 0.1
 
-
+df_array = Array(DataFrame,0)
 for i = 1:100
   run(`build.bat >nul`)
-  #traci.start(["sumo", "-c", "i3.sumocfg"], label="sim1")
-  #traci.start(["sumo", "-c", "i3.sumocfg"], label="sim2")
-  traci.start(["sumo", "-c" , "i3.sumocfg"])
-  #traci.switch("sim1")
+  traci.start(["sumo-gui", "-c" , "i3.sumocfg"])
   traci.simulationStep()
   traci.vehicle[:moveToXY]("ego1","bottom_in", 0, pos_i[1], pos_i[2] - 6, 1)
   traci.vehicle[:setSpeedMode]("ego1", 01100)
-  #traci.switch("sim2")
-  #traci.simulationStep()
-  #traci.vehicle[:moveToXY]("ego1","bottom_in", 0, pos_i[1], pos_i[2] - 6, 1)
-  #traci.vehicle[:setSpeedMode]("ego1", 01100)
   step = 0
   last_step = 0
   go = 180
   oncoming_cars = Array(String,0)
+
   end_dists = Array(Float64,n_tracked_cars,2)
+  df = DataFrame(dist = Float64[], speed = Float64[], headway = Float64[])
   while true
-     step += 1
-     traci.simulationStep();
-     pos_ego = traci.vehicle[:getPosition]("ego1")
-     dist = traci.simulation[:getDistance2D](pos_ego[1], pos_ego[2], pos_i[1], pos_i[2])
-     if step < go
-       traci.vehicle[:slowDown]("ego1", 0, 100);
 
-     elseif step == go
-       vehicles = traci.vehicle[:getIDList]()
-       if length(vehicles) < 2
-         break;
-       end
-       dists = Float64[]
-       for vehicle in vehicles
-         if vehicle == "ego1"
-           continue
-         end
-         pos = traci.vehicle[:getPosition](vehicle)
-         push!(dists, traci.simulation[:getDistance2D](pos[1], pos[2], pos_i[1], pos_i[2]))
-       end
-       dists_sort = sort(dists)
-       for i = 1:n_tracked_cars
-         push!(oncoming_cars, vehicles[find(x -> x == dists_sort[i],dists)][1])
-       end
-       traci.vehicle[:slowDown]("ego1", 20, 5000);
-     else
-       traci.vehicle[:slowDown]("ego1", 20, 5000);
-       #check for colision
+    step += 1
+    traci.simulationStep();
+    pos_ego = traci.vehicle[:getPosition]("ego1")
+    dist = traci.simulation[:getDistance2D](pos_ego[1], pos_ego[2], pos_i[1], pos_i[2])
+
+    # at step 130, it finds the closes cars and starts gathering data on them
+    if step == 130
+      vehicles = traci.vehicle[:getIDList]()
+      dists = Float64[]
+      for vehicle in vehicles
+        if vehicle == "ego1"
+          continue
+        end
+        pos = traci.vehicle[:getPosition](vehicle)
+        push!(dists, traci.simulation[:getDistance2D](pos[1], pos[2], pos_i[1], pos_i[2]))
+      end
+      dists_sort = sort(dists)
+      for i = 1:n_tracked_cars
+        push!(oncoming_cars, vehicles[find(x -> x == dists_sort[i],dists)][1])
+      end
+    end
 
 
 
+    if step < go
+      traci.vehicle[:slowDown]("ego1", 0, 100);
+    elseif step == go
+      traci.vehicle[:slowDown]("ego1", 20, 5000);
+    else
+      traci.vehicle[:slowDown]("ego1", 20, 5000);
+      #check for colision
 
 
+
+    end
+    if dist > 25
+     i = 0
+
+     #finds the final distance for the cars
+     for vehicle in oncoming_cars
+       println(typeof(vehicle))
+       i += 1
+       pos = traci.vehicle[:getPosition](vehicle)
+       end_dists[i,1] = dist = traci.simulation[:getDistance2D](pos[1], pos[2], pos_i[1], pos_i[2])
      end
-     if dist > 25
-       i = 0
-       for vehicle in oncoming_cars
-         println(typeof(vehicle))
-         i += 1
-         pos = traci.vehicle[:getPosition](vehicle)
-         end_dists[i,1] = dist = traci.simulation[:getDistance2D](pos[1], pos[2], pos_i[1], pos_i[2])
-       end
-       last_step = step
-       break
-     end
-   end
+     last_step = step
+     break
+    end
+    end
   traci.close()
 
   #now simulate again, but without having the car go
