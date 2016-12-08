4a40201834e4996c669e0eeef7a3fdef7100847b
djp42 <djp42@stanford.edu>
2016-12-08 12:15:38 -0800

    added reward saving

--- intersection3/build.bat
+++ intersection3/build.bat
@@ -1,2 +1,2 @@
 netconvert --node-files=i3.nod.xml --edge-files=i3.edg.xml --output-file=i3.net.xml
-jtrrouter --flow-files=i3.flows.xml --turn-ratio-files=i3.turns.xml --net-file=i3.net.xml --output-file=i3.rou.xml --begin 0 --end 40 --random true
+jtrrouter --flow-files=i3.flows.xml --turn-ratio-files=i3.turns.xml --net-file=i3.net.xml --output-file=i3.rou.xml --begin 0 --end 1000 --random true
