21a21893402f26fd0246be1b85483960a060be69
johnldean <deanjl@stanford.edu>
2016-12-10 11:41:50 -0800

    updating with some stuff

--- intersection3/analysis/state_histograms.jl
+++ intersection3/analysis/state_histograms.jl
@@ -1,19 +1,27 @@
 using DataFrames
 using Discretizers
 using Distributions
-#using PyPlot
+using PyPlot
 
 
 states = readtable("simulated_states.csv")
 
 for i = 1:4
   data = states[i]
-  data = data[find(x -> x < 800,data)]
-  n = 5
+  data = data[find(x -> x < 800, data)]
+  n = 4
   if i <= 2
-    n = 6
+    n = 5
   end
-  bins = binedges(DiscretizeUniformWidth(n),data)
+  bins = binedges(DiscretizeUniformCount(n),data)
   println(bins)
-  #h = PyPlot.plt[:hist](data,50)
+  h = PyPlot.plt[:hist](data,50)
+  sleep(10)
 end
+
+
+#data = readcsv("analysis/rewards_results_of_working2.policy")
+# data = convert(Array{Float64,1},reshape(data,length(data)[1]))
+# data = data[find(x -> x > -9000,data)]
+# PyPlot.plt[:hist](data,40)
+# writecsv("rewards_working2_noColisions.dat",data)
